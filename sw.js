45c0c2eafba786add23e59b9e6fd19ffd427fbcb
brotherdan-bit <jornalasemente@gmail.com>
2026-09-13 00:41:53 -0300

    Adiciona favicon da aba (link rel=icon); sw cache v21

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    quando há rede, baixa a versão nova em segundo plano para a
    próxima abertura. */
 
-const CACHE = 'sermonario-v20';
+const CACHE = 'sermonario-v21';
 const ASSETS = [
   './',
   './index.html',
